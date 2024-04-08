eeac82c5d06f9b12aead307d864842660c064c2b
Guilherme Trevisan Linhares <91161693+GTL98@users.noreply.github.com>
2024-04-08 12:29:24 -0300

    Add files via upload

---
+++ Python Village/02_INI2/INI2.py
@@ -0,0 +1,22 @@
+# --- Fornecido: Dois números interos positivos a e b menor do que 1000 --- #
+# --- Problema: O inteiro correspondente ao quadrado da hipotenusa do triângulo, cujo os lados são a e b --- #
+
+# --- Indicar o arquivo --- #
+arquivo = 'rosalind_ini2.txt'
+
+# --- Abrir o arquivo --- #
+with open(arquivo, 'r') as txt:
+    # --- Obter o conteúdo do arquivo --- #
+    conteudo = txt.read()
+
+    # --- Obter o primeiro número --- #
+    a = int(conteudo.split(' ')[0])
+
+    # --- Obter o segundo número --- #
+    b = int(conteudo.split(' ')[1])
+
+# --- Encontrar a hipotenusa do triângulo --- #
+h = (a**2 + b**2)
+
+# --- Mostrar a respota --- #
+print(h)
