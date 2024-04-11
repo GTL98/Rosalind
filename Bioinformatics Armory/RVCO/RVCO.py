8b5ca908922c7b746e6e574b13187d282c9f25b4
Guilherme Trevisan Linhares <91161693+GTL98@users.noreply.github.com>
2024-04-11 14:19:27 -0300

    Add files via upload

---
+++ Bioinformatics Armory/RVCO/RVCO.py
@@ -0,0 +1,27 @@
+# --- Dado: Coleção de n (n <= 10) strings de DNA --- #
+# --- Problema: O número de strings que são iguais no modo normal e no reverso complementar --- #
+
+# --- Importar a biblioteca --- #
+from Bio import SeqIO
+
+# --- Indicar o arquivo --- #
+arquivo = 'rosalind_rvco.txt'
+
+# --- Criar o contador --- #
+contador = 0
+
+# --- Ler o arquivo com o Biopython --- #
+for registro in SeqIO.parse(arquivo, 'fasta'):
+    # --- Obter a sequência --- #
+    sequencia = registro.seq
+
+    # --- Obter a sequência reversa complementar --- #
+    sequencia_reversa_complementar = sequencia.reverse_complement()
+
+    # --- Verificar se a sequência reversa complementar é igual a original --- #
+    if sequencia_reversa_complementar == sequencia:
+        # --- Se for igual, o contador aumenta 1 --- #
+        contador += 1
+
+# --- Mostrar a resposta --- #
+print(contador)
