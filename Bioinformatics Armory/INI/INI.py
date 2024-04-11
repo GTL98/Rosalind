90f94fd56aa5cfdbf0bfe07d7a8e0e4255b62c4d
Guilherme Trevisan Linhares <91161693+GTL98@users.noreply.github.com>
2024-04-11 10:59:17 -0300

    Add files via upload

---
+++ Bioinformatics Armory/INI/INI.py
@@ -0,0 +1,25 @@
+# --- Fornecido: Uma string s de DNA com até 1000 nucleotídeos --- #
+# --- Problema: A quantidade de cada nucleotídeo em s, separado por espaço. A ordem é adenina (A), citosina (C), guanina (G) e timina (T) --- #
+
+# --- Indicar o arquivo --- #
+arquivo = 'rosalind_ini.txt'
+
+# --- Abrir o arquivo --- #
+with open(arquivo, 'r') as txt:
+    # --- Obter o conteúdo do arquivo --- #
+    s = txt.read()
+
+# --- Contar quantas adeninas há em s --- #
+A = s.count('A')
+
+# --- Contar quantas citosinas há em s --- #
+C = s.count('C')
+
+# --- Contar quantas guaninas há em s --- #
+G = s.count('G')
+
+# --- Contar quantas timinas há em s --- #
+T = s.count('T')
+
+# --- Mostrar a resposta --- #
+print(f'{A} {C} {G} {T}')
