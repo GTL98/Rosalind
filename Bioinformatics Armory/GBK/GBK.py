cbd4ae3fb5b6323810f63699059a857a81eb7f47
Guilherme Trevisan Linhares <91161693+GTL98@users.noreply.github.com>
2024-04-11 11:37:33 -0300

    Add files via upload

---
+++ Bioinformatics Armory/GBK/GBK.py
@@ -0,0 +1,40 @@
+# --- Fornecido: O gênero de uma espécie seguido de duas datas no formaro AAAA/M/D --- #
+# --- Problema: A quantidade de entradas de nucleotídeos GenBank do gênero durante o período informado --- #
+
+# --- Importar a biblioteca --- #
+from Bio import Entrez
+
+# --- Indicar o arquivo --- #
+arquivo = 'rosalind_gbk.txt'
+
+# --- Abrir o arquivo --- #
+with open(arquivo, 'r') as txt:
+    # --- Obter o conteudo do arquivo --- #
+    conteudo = txt.readlines()
+
+    # --- Obter o gênero --- #
+    genero = conteudo[0].strip()
+
+    # --- Obter a data inicial --- #
+    data_inicial = conteudo[1].strip()
+
+    # --- Obter a data final --- 3
+    data_final = conteudo[2].strip()
+
+# --- Indicar um e-mail aleatório --- #
+Entrez.email = 'your.email@example.com'
+
+# --- Realizar a busca no banco de dados --- #
+busca = Entrez.esearch(
+    db='nucleotide',
+    term=f'"({genero}"[Organism]) AND ("{data_inicial}"[Publication Date]:"{data_final}"[Publication Date])'
+    )
+
+# --- Ler a busca --- #
+registro = Entrez.read(busca)
+
+# --- Saber quantas entradas há --- #
+quantidade = registro['Count']
+
+# --- Mostrar a resposta --- #
+print(quantidade)
