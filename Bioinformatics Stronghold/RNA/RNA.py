035aada058779448d977d651e1a821d9e0ccb399
Guilherme Trevisan Linhares <91161693+GTL98@users.noreply.github.com>
2024-04-18 11:07:13 -0300

    Add files via upload

---
+++ Bioinformatics Stronghold/RNA/RNA.py
@@ -0,0 +1,16 @@
+# --- Dado: uma string de DNA com no máximo 1000 nucleotídeos --- #
+# --- Problema: A string de RNA traduzida --- #
+
+# --- Indicar o arquivo --- #
+arquivo = 'rosalind_rna.txt'
+
+# --- Abrir o arquivo --- #
+with open(arquivo, 'r') as txt:
+    # --- Obter a string de DNA --- #
+    dna = txt.read()
+
+# --- Trocar a timina (T) por uracila (U) --- #
+rna = dna.replace('T', 'U')
+
+# --- Mostrar a resposta --- #
+print(rna)
