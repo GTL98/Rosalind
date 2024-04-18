f665a44d17967fbecdcb0ca8a7a3cb29ab02299f
Guilherme Trevisan Linhares <91161693+GTL98@users.noreply.github.com>
2024-04-18 11:00:19 -0300

    Add files via upload

---
+++ Bioinformatics Stronghold/DNA/DNA.py
@@ -0,0 +1,25 @@
+# --- Dado: Uma string s de até 1000 nucleotídeos --- #
+# --- Problema: A quantidade de quantas vezes cada nucleotídeo aparece na string (A, C, G e T) --- #
+
+# --- Indicar o arquivo --- #
+arquivo = 'rosalind_dna.txt'
+
+# --- Ler o arquivo --- #
+with open(arquivo, 'r') as txt:
+    # --- Obter a string --- #
+    dna = txt.read()
+
+# --- Contar quantas adeninas há na string --- #
+adenina = dna.count('A')
+
+# --- Contar quantas citosinas há na string --- #
+citosina = dna.count('C')
+
+# --- Contar quantas guanina há na string --- #
+guanina = dna.count('G')
+
+# --- Contar quantas timinas há na string --- #
+timina = dna.count('T')
+
+# --- Mostrar a resposta --- #
+print(adenina, citosina, guanina, timina)
