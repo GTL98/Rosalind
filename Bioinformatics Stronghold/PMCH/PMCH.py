04bc6f821d8d315c8355890c0a6d365f35ea02ad
Guilherme Trevisan Linhares <91161693+GTL98@users.noreply.github.com>
2024-05-14 11:20:15 -0300

    Add files via upload

---
+++ Bioinformatics Stronghold/PMCH/PMCH.py
@@ -0,0 +1,30 @@
+# --- Dado: Uma string de s de RNA de até 80 nucleotídeos com as mesmas quantidades de A com U e G com C --- #
+# --- Problema: O número total de pareamentos perfeitosde s --- #
+
+# --- Importar a biblioteca --- #
+from Bio import SeqIO
+from math import factorial
+
+# --- Informar o arquivo --- #
+arquivo = 'rosalind_pmch.txt'
+
+# --- Obter a quantidade dos pares de nucleotídeos --- #
+A_U = 0
+G_C = 0
+for registro in SeqIO.parse(arquivo, 'fasta'):
+    # --- Obter a sequência --- #
+    sequencia = registro.seq
+
+    # --- Contagem de A e U --- #
+    A = sequencia.count('A')
+    A_U = A
+
+    # --- Contagem de G e C --- #
+    G = sequencia.count('G')
+    G_C = G
+
+# --- Pareamentos --- #
+pareamentos = factorial(A_U) * factorial(G_C)
+
+# --- Mostrar a resposta --- #
+print(factorial(A_U) * factorial(G_C))
